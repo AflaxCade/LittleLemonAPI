3b76e770c5a38545d1bc217bd35c4bc5fd37d784
AflaxCade <aflah.cade@gmail.com>
2024-10-15 14:20:50 +0300

    modified the models.py file and migrated

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -4,14 +4,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(default="", null=False)
     title = models.CharField(max_length=255, db_index=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
     
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
     
 class cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
