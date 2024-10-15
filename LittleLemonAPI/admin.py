568ea19a13604806b1398499d3909a25448a3f14
AflaxCade <aflah.cade@gmail.com>
2024-10-15 14:19:55 +0300

    Added Django admin configurations for models and Setup custom admin views for Category and MenuItem, including list display, filters, and search fields

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, MenuItem, cart, Order, OrderItem
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    ordering = ["id"]
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "price", "featured", "category")
+    list_filter = ("featured", "category")
+    search_fields = ("title", "category")
+    ordering = ["id"]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
+admin.site.register(cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
